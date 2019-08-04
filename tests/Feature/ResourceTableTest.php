626fa401fcf1891a29c7053bc10599a1b580179e
Miras <miras.mustimov@tuta.io>
2019-08-04 13:58:39 +0600

    resource_limit_defaults_to_10

--- tests/Feature/ResourceTableTest.php
+++ tests/Feature/ResourceTableTest.php
@@ -13,19 +13,43 @@ class ResourceTableTest extends TestCase
 {
     use WithFaker, RefreshDatabase;
 
+    public $users;
+
     public function setUp() :void
     {
+        parent::setUp();
+
         $this->withoutExceptionHandling();
+
+        $this->users = factory(User::class, 30)->create();
     }
 
     /** @test */
     function can_render_resource_list()
     {
-        $users = factory(User::class, 30)->create();
+        $this
+            ->table([ 'resource' => 'users' ])
+            ->assertSee($this->users[0]->name);
+    }
 
-        Livewire::test(ResourceTable::class, [
-                'resource' => 'users'
-            ])
-            ->assertSee($users[0]->name);
+    /** @test */
+    function resource_limit_defaults_to_10()
+    {
+        $this->users->take(10)->each(function ($user) {
+            $this
+                ->table([ 'resource' => 'users' ])
+                ->assertSee($user->name);
+        });
+
+        $this->users->reverse()->take(20)->each(function ($user) {
+            $this
+                ->table([ 'resource' => 'users' ])
+                ->assertDontSee($user->name);
+        });
+    }
+
+    protected function table($options)
+    {
+        return Livewire::test(ResourceTable::class, $options);
     }
 }
