15438c5fe1b8a8bfed96ac11025a6f89204485f1
Miras <miras.mustimov@tuta.io>
2019-08-04 13:19:33 +0600

    create livewire component

---
+++ app/Http/Livewire/ResourceTable.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire;
+
+use Livewire\Component;
+
+class ResourceTable extends Component
+{
+    public function render()
+    {
+        return view('livewire.resource-table');
+    }
+}
